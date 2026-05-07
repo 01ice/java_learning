f5d6817ab958ba54a3f9e6223dc4f8c1953116b2
01ice <165007414+01ice@users.noreply.github.com>
2026-05-07 23:35:34 +0800

    课堂代码

--- 26_5_7/src/HelloWorld.java
+++ 26_5_7/src/HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println(10);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println('a');
     }
+    //一般注释
+    /*
+    多行注释
+     */
+    /**
+     * 文档注释
+     */
 }
